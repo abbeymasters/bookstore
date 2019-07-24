5d22b64982c945fedb1665513c8e017a86408aaa
Abbey Masters <abbeykmasters@gmail.com>
2019-07-24 11:35:32 -0700

    working updates

--- src/shopping-cart.js
+++ src/shopping-cart.js
@@ -1,17 +1,17 @@
 import books from './data/books.js';
-import order from './data/order.js';
+import cart from './data/order.js';
 import renderLineItem from './render-line-item.js';
 import findBook from './register.js';
-import { getOrderTotal } from './register.js';
+import { orderTotal } from './register.js';
 
 // locate the table element where your products will go
 const tbody = document.querySelector('tbody');
 
 // loop through your data
-for(let i = 0; i < order.lenth; i++) {
+for(let i = 0; i < cart.length; i++) {
 
 // Create a variable for that is the singular of your domain list
-    const lineItem = order[i];
+    const lineItem = cart[i];
 
 // Create a variable that is the found product for that line item
     const book = findBook(books, lineItem.code);
@@ -23,8 +23,8 @@ for(let i = 0; i < order.lenth; i++) {
 }
 
 // locate the table cell in the footer where the order total will go
-    const tfoot = document.querySelector('order-total-cell');
+    const orderTotalCell = document.getElementById('order-total-cell');
 
-// use the getOrderTotal function to calculate the order total and display
-    const finalOrder = getOrderTotal(lineItem, book);
-    tfoot.appendChild(finalOrder);
+// use the getOrderTotal function to calculate the order total
+    const finalTotal = orderTotal(cart, books).toLocaleString('en-US', { style: 'currency', currency: 'USD' });;
+    orderTotalCell.textContent = finalTotal;
